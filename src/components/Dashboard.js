2b70396db49ffc52133c8e8d86a7ad5b727dde36
Shaneen Vitug <shaneenvitug@gmail.com>
2019-11-23 18:42:54 +1100

    Add some Material UI for text fields and make component use React Hooks

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,127 +1,168 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
 // const SERVER_URL = 'https://tuesday-server.herokuapp.com/tasks'
 const SERVER_URL = "http://localhost:3000/groups"
 
-export default class Dashboard extends Component {
-    
-    state = {
-        groups: [],
-        groupName: ''
-    }
+const useStyles = makeStyles(theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200,
+    },
+}));
 
-    componentDidMount() {
+function Dashboard(props) {
+
+    const [groups, setGroups] = useState([]);
+    const [groupName, setGroupName] = useState('');
+
+    const classes = useStyles();
+
+    useEffect(() => {
         axios.get(SERVER_URL).then((results) => {
-            this.setState({groups: results.data});
+            setGroups(results.data);
         })
-    }
+    }, [])
 
-    saveGroupName = (event) => {
+    const saveGroupName = (event) => {
         event.preventDefault();
-        console.log("groupName", this.state.groupName)
 
         const postRequest = {
-            "name": this.state.groupName,
+            "name": groupName,
             "tasks": []
           }
 
         axios.post(SERVER_URL, postRequest).then((result) => {
            
             axios.get(SERVER_URL).then((results) => {
-                this.setState({ groups: results.data });
+                setGroups(results.data);
             })
 
         })
     }
 
     // When you type or edit a task it will get updated/saved
-    onChangeHandler = (event, { id }, task, taskIndex, field) => {
+    const onChangeHandler = (event, { id }, task, taskIndex, field) => {
         event.persist() // Need this, read https://reactjs.org/docs/events.html#event-pooling 
+
+        // NOTE TODO
+        // This should actually be done when onBlur
+        // And not onChange, only need to call it when the user leaves the field to do something else
+        // And not on every keystroke
+
+        const selectedGroupIndex = groups.findIndex((group) => group.id === id)
+
+        const updatedGroups = groups[selectedGroupIndex].tasks[taskIndex][field] = event.target.value
+
+        setGroups(updatedGroups)
+
+        const updatedGroup = groups.find((group) => group.id === id)
+
+        axios.put(`${SERVER_URL}/${id}`, updatedGroup).then((result) => {
         
-        this.setState((prevState) => {
-            const selectedGroupIndex = prevState.groups.findIndex((group) => group.id === id)
-
-            const updatedGroups = prevState.groups
-            updatedGroups[selectedGroupIndex].tasks[taskIndex][field] = event.target.value
-
-            return ({ groups: updatedGroups })
-
-        }, () => {
-            // This second anonymous function is a callback function which is called after a
-            // successful setState call
-
-            // NOTE TODO
-            // This should actually be done when onBlur
-            // And not onChange, only need to call it when the user leaves the field to do something else
-            // And not on every keystroke
-
-            const updatedGroup = this.state.groups.find((group) => group.id === id)
-
-            axios.put(`${SERVER_URL}/${id}`, updatedGroup).then((result) => {
-           
-                axios.get(SERVER_URL).then((results) => {
-                    this.setState({ groups: results.data });
-                })
-            })
-        })
-    }
-
-    deleteGroup = (event, group) => {
-        axios.delete(`${SERVER_URL}/${group.id}`).then((result) => {
-           
             axios.get(SERVER_URL).then((results) => {
-                this.setState({ groups: results.data });
+                setGroups(results.data);
             })
         })
     }
 
-    render() {
-        if (!this.state.groups.length) return (<h3>Loading</h3>);
-
-        return(
-            <div>
-                <form onSubmit={this.saveGroupName}>
-                    <input placeholder="Group Name" onChange={(event) => this.setState({ groupName: event.target.value })} />
-                    <input type="submit" value="Add Group" />
-                </form>
-
-                {this.state.groups.map(group => (
-                    <div>
-                        <table>
-                            <button onClick={(event) => this.deleteGroup(event, group)}>x</button>
-                            <tr>
-                                <th>{group.name}</th>
-                                <th>Owner</th>
-                                <th>Status</th>
-                                <th>Due Date</th>
-                                <th>Priority</th>
-                            </tr>
-                            
-                            {group.tasks.map((task, taskIndex) => (
-                                <tr onClick={this.onClickHandler}>
-                                    <td>
-                                        <input type="text" value={task.name} onChange={(event) => this.onChangeHandler(event, group, task, taskIndex, "name")} />
-                                    </td>
-                                    <td>
-                                        <input type="text" value={task.owner} />
-                                    </td>
-                                    <td>
-                                        <input type="text" value={task.status} />
-                                    </td>
-                                    <td>
-                                        <input type="text" value={task.dueDate} />
-                                    </td>
-                                    <td>
-                                        <input type="text" value={task.priority} />
-                                    </td>
-                                </tr>
-                            ))}
-                            
-                        </table>
-                    </div>
-                ))}
-            </div>
-        )
+    const deleteGroup = (event, group) => {
+        axios.delete(`${SERVER_URL}/${group.id}`).then((result) => {
+            axios.get(SERVER_URL).then((results) => {
+                setGroups(results.data);
+            })
+        })
     }
+
+    return (
+        <div>
+            { !groups.length ? <h3>Loading</h3> : (
+                <div>
+                    <form onSubmit={saveGroupName}>
+                        <input placeholder="Group Name" onChange={(event) => setGroupName(event.target.value)} />
+                        <input type="submit" value="Add Group" />
+                    </form>
+
+                    { groups.map(group => (
+                        <div>
+                            <table>
+                                <button onClick={(event) => deleteGroup(event, group)}>x</button>
+                                <tr>
+                                    <th>{group.name}</th>
+                                    <th>Owner</th>
+                                    <th>Status</th>
+                                    <th>Due Date</th>
+                                    <th>Priority</th>
+                                </tr>
+                                
+                                { group.tasks.map((task, taskIndex) => (
+                                    <tr>
+                                        <td>
+                                            <TextField
+                                                id="filled-read-only-input"
+                                                value={task.name}
+                                                className={classes.textField}
+                                                margin="normal"
+                                                variant="outlined"
+                                                // onChange={(event) => onChangeHandler(event, group, task, taskIndex, "name")}
+                                            />
+                                        </td>
+                                        <td>
+                                            <TextField
+                                                id="filled-read-only-input"
+                                                value={task.owner}
+                                                className={classes.textField}
+                                                margin="normal"
+                                                variant="outlined"
+                                                // onChange={(event) => onChangeHandler(event, group, task, taskIndex, "name")}
+                                            />
+                                        </td>
+                                        <td>
+                                            <TextField
+                                                id="filled-read-only-input"
+                                                value={task.status}
+                                                className={classes.textField}
+                                                margin="normal"
+                                                variant="outlined"
+                                                // onChange={(event) => onChangeHandler(event, group, task, taskIndex, "name")}
+                                            />
+                                        </td>
+                                        <td>
+                                            <TextField
+                                                id="filled-read-only-input"
+                                                value={task.dueDate}
+                                                className={classes.textField}
+                                                margin="normal"
+                                                variant="outlined"
+                                                // onChange={(event) => onChangeHandler(event, group, task, taskIndex, "name")}
+                                            />
+                                        </td>
+                                        <td>
+                                            <TextField
+                                                id="filled-read-only-input"
+                                                value={task.priority}
+                                                className={classes.textField}
+                                                margin="normal"
+                                                variant="outlined"
+                                                // onChange={(event) => onChangeHandler(event, group, task, taskIndex, "name")}
+                                            />
+                                        </td>
+                                    </tr>
+                                ))}   
+                            </table>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    )
 }
+
+export default Dashboard;

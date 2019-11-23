6cb772154ef79e6278015c6908966d7fa51a9996
Shaneen Vitug <shaneenvitug@gmail.com>
2019-11-23 12:08:05 +1100

    render mock api

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Dashboard = props => {
-    return(
-        <div>
+const SERVER_URL = 'http://localhost:3000/groups'
+
+export default class Dashboard extends Component {
+    
+    state = {
+        groups: []
+    }
+
+    componentDidMount() {
+        console.log("component did run when loaded");
+        axios.get(SERVER_URL).then((results) => {
+            console.log(results.data);
+            
+            this.setState({groups: results.data});
+            
+        })
+    }
+
+    render() {
+        if (!this.state.groups.length) return (<h3>Loading</h3>);
+
+        return(
             <div>
-                <h1>Dashboard</h1>
-                <h1>Status: {props.loggedInStatus}</h1>
+                {this.state.groups.map(group => (
+                    <div>
+                        <h2>{group.name}</h2>
+                        {group.tasks.map(task => (
+                                <p>Task: {task.name} | Status: {task.status} | Due date: {task.dueDate} | Priority: {task.priority} | Owner: {task.owner}</p>
+                        ))}
+                    </div>
+                ))}
             </div>
-        </div>
-    )
-}
+        )
+    }
 
-export default Dashboard
+
+
+}

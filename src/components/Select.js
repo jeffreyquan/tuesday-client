891117e38d0fc292f456d95ceadbcffb0aa07d07
Shaneen Vitug <shaneenvitug@gmail.com>
2019-11-27 20:24:03 +1100

    select dropdown for status and priority

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const Select = (props) => {
+  console.log(props, 'props');
+  
   return(
     <div className="form-group">
       <select
         name={props.name}
         value={props.value}
-        onChange={props.handleChange}
+        onChange={props.onChange}
         >
         <option value="">{props.placeholder}</option>
         {props.options.map(option => {

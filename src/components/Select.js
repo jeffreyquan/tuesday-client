58bf83f98e26449206088e5be5a2e998943e3359
Shaneen Vitug <shaneenvitug@gmail.com>
2019-11-27 16:06:28 +1100

    edit owner done, still need to fix select dropdown

---
+++ src/components/Select.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Select = (props) => {
+  return(
+    <div className="form-group">
+      <select
+        name={props.name}
+        value={props.value}
+        onChange={props.handleChange}
+        >
+        <option value="">{props.placeholder}</option>
+        {props.options.map(option => {
+          return(
+            <option
+              key={option}
+              value={option}
+              label={option}>{option}
+              </option>
+          );
+        })}
+        </select>
+    </div>)
+}
+
+export default Select;
--- src/components/Task.js
+++ src/components/Task.js
@@ -2,8 +2,7 @@ import React, { useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
-
-const SERVER_URL = "http://localhost:3000/projects/1"
+import Select from './Select'
 
 let URL = (model, id = '') => {
   return `http://localhost:3000/${model}/${id}`
@@ -25,8 +24,26 @@ const useStyles = makeStyles(theme => ({
 export default function Task(props) {
   const classes = useStyles();
   const [task, setTask] = useState(props.task)
+  const [statusOptions, setStatusOptions] = useState(['Done', 'Working On It', 'Stuck'])
+  const [taskStatus, setTaskStatus] = useState(props.task.status)
+  const [priorityOptions, setPriorityOptions] = useState(['High', 'Medium', 'Low'])
+  const [taskPriority, setTaskPriority] = useState(props.task.priority)
+  const [owner, setOwner] = useState(props.task.owner)
+
   const updateTaskName = () => {
     axios.put(URL('tasks', props.id), { name: task, group_id: props.group_id })
+  }
+
+  const updateStatus = () => {
+    axios.put(URL('tasks', props.id), { status: taskStatus, group_id: props.group_id })
+  }
+
+  const updatePriority = () => {
+    axios.put(URL('tasks', props.id), { priority: taskPriority, group_id: props.group_id })
+  }
+
+  const updateOwner = () => {
+    axios.put(URL('tasks', props.id), { owner: owner, group_id: props.group_id })
   }
 
   return (
@@ -45,20 +62,23 @@ export default function Task(props) {
       </td>
       <td>
           <TextField
+              onChange={(event) => setOwner(event.target.value)}
+              onBlur={updateOwner}
               id="filled-read-only-input"
-              value={props.task.owner}
+              value={owner}
               className={classes.textField}
               margin="normal"
               variant="outlined"
           />
       </td>
       <td>
-          <TextField
-              id="filled-read-only-input"
-              value={props.task.status}
-              className={classes.textField}
-              margin="normal"
-              variant="outlined"
+          <Select
+              name={'status'}
+              options={statusOptions}
+              value={taskStatus}
+              placeholder={''}
+              onChange={(event => setTaskStatus(event.target.value))}
+              onBlur={updateStatus}
           />
       </td>
       <td>
@@ -71,12 +91,13 @@ export default function Task(props) {
           />
       </td>
       <td>
-          <TextField
-              id="filled-read-only-input"
-              value={props.task.priority}
-              className={classes.textField}
-              margin="normal"
-              variant="outlined"
+          <Select
+              name={'priority'}
+              options={priorityOptions}
+              value={taskPriority}
+              placeholder={''}
+              onChange={(event => setTaskPriority(event.target.value))}
+              onBlur={updatePriority}
           />
       </td>
     </tr>

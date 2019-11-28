afa040b2365426d6d50bb2b48fb373656dcd7d43
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-11-28 21:31:32 +1100

    add email format validation

--- src/components/Team.js
+++ src/components/Team.js
@@ -200,6 +200,8 @@ class InviteForm extends Component {
     let alreadyInvited = this.state.emails;
     if (!value) {
       errorMessage = "Please enter email.";
+    }  else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+      errorMessage = "Please enter valid email.";
     } else if (alreadyInvited.includes(value)) {
       errorMessage = "Already invited."
     }
